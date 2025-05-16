a2803bc4bd47ede213cddf6e724d28cb01b02bdd
Stavros <schampilomatis@gmail.com>
2025-05-16 14:59:48 +0200

    add lazygit

--- init.lua
+++ init.lua
@@ -478,7 +478,7 @@ require('lazy').setup({
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.neo-tree',
-  require 'kickstart.plugins.gitsigns',
+  require 'kickstart.plugins.git',
   { import = 'custom.plugins' },
 }, {
   ui = {
