8ba58e81ee8b192efe55824dff26e61cf8559e3a
Stavros Champilomatis <stavros.champilomatis@booking.com>
2025-07-03 15:57:08 +0200

    visual multi

--- init.lua
+++ init.lua
@@ -490,6 +490,34 @@ require('lazy').setup({
     },
     opts = {},
   },
+  {
+    'mg979/vim-visual-multi',
+    -- See https://github.com/mg979/vim-visual-multi/issues/241
+    init = function()
+      vim.g.VM_default_mappings = 0
+      vim.g.VM_maps = {
+        ['Find Under'] = '',
+      }
+      vim.g.VM_add_cursor_at_pos_no_mappings = 1
+      local function visual_cursors_with_delay()
+        -- Execute the vm-visual-cursors command.
+        vim.cmd 'silent! execute "normal! \\<Plug>(VM-Visual-Cursors)"'
+        -- Introduce delay via VimScript's 'sleep' (set to 500 milliseconds here).
+        vim.cmd 'sleep 200m'
+        -- Press 'A' in normal mode after the delay.
+        vim.cmd 'silent! execute "normal! A"'
+      end
+      local wk = require 'which-key'
+      wk.add {
+        { '<leader>m', group = 'Visual Multi' },
+        { '<leader>ma', '<Plug>(VM-Select-All)<Tab>', desc = 'Select All' },
+        { '<leader>mo', '<Plug>(VM-Toggle-Mappings)', desc = 'Toggle Mapping' },
+        { '<leader>mp', '<Plug>(VM-Add-Cursor-At-Pos)', desc = 'Add Cursor At Pos' },
+        { '<leader>mr', '<Plug>(VM-Start-Regex-Search)', desc = 'Start Regex Search' },
+        { '<leader>mv', visual_cursors_with_delay, desc = 'Visual Cursors', mode = 'v' },
+      }
+    end,
+  },
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.neo-tree',
