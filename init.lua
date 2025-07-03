3cee2de2cde90dd026f17cb25ba35f3ba6a51bec
Stavros Champilomatis <stavros.champilomatis@booking.com>
2025-07-03 16:03:01 +0200

    the function was unnecessary

--- init.lua
+++ init.lua
@@ -499,14 +499,6 @@ require('lazy').setup({
         ['Find Under'] = '',
       }
       vim.g.VM_add_cursor_at_pos_no_mappings = 1
-      local function visual_cursors_with_delay()
-        -- Execute the vm-visual-cursors command.
-        vim.cmd 'silent! execute "normal! \\<Plug>(VM-Visual-Cursors)"'
-        -- Introduce delay via VimScript's 'sleep' (set to 500 milliseconds here).
-        vim.cmd 'sleep 200m'
-        -- Press 'A' in normal mode after the delay.
-        vim.cmd 'silent! execute "normal! A"'
-      end
       local wk = require 'which-key'
       wk.add {
         { '<leader>m', group = 'Visual Multi' },
@@ -514,7 +506,7 @@ require('lazy').setup({
         { '<leader>mo', '<Plug>(VM-Toggle-Mappings)', desc = 'Toggle Mapping' },
         { '<leader>mp', '<Plug>(VM-Add-Cursor-At-Pos)', desc = 'Add Cursor At Pos' },
         { '<leader>mr', '<Plug>(VM-Start-Regex-Search)', desc = 'Start Regex Search' },
-        { '<leader>mv', visual_cursors_with_delay, desc = 'Visual Cursors', mode = 'v' },
+        { '<leader>mv', '<Plug>(VM-Visual-Cursors)', desc = 'Visual Cursors', mode = 'v' },
       }
     end,
   },
